b0e7ff50da11b8a11bf53a8bc318001cff0ad728
jonsia cejohn <jonsiacejohn@gmail.com>
2026-06-25 13:26:09 +0530

    Fix: hardcode backend URL

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://blog-platform-nbwn.onrender.com/api',
 });
 
 api.interceptors.request.use(config => {
